2adc3cd89bfb5fba7ec68c6c8e2e28f6f6d3e021
bentiedt <ben@getben.com>
2022-12-09 17:39:19 -0500

    First default build for Community

--- packages/ckeditor5-build-classic/src/ckeditor.js
+++ packages/ckeditor5-build-classic/src/ckeditor.js
@@ -7,62 +7,104 @@
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
-import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Clipboard from '@ckeditor/ckeditor5-clipboard/src/clipboard';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code';
+import FindAndReplace from'@ckeditor/ckeditor5-find-and-replace/src/findandreplace';
+import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily';
+import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor';
+import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor';
+import FontSize from '@ckeditor/ckeditor5-font/src/fontsize';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import CKBox from '@ckeditor/ckeditor5-ckbox/src/ckbox';
-import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
-import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline';
 import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
 import Indent from '@ckeditor/ckeditor5-indent/src/indent';
+import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import Link from '@ckeditor/ckeditor5-link/src/link';
+import LinkImage from '@ckeditor/ckeditor5-link/src/linkimage';
 import List from '@ckeditor/ckeditor5-list/src/list';
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
+import ListProperties from '@ckeditor/ckeditor5-list/src/listproperties';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
-import PictureEditing from '@ckeditor/ckeditor5-image/src/pictureediting';
+import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat';
+import SelectAll from '@ckeditor/ckeditor5-select-all/src/selectall';
+import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting';
+import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharacters';
+import SpecialCharactersArrows from '@ckeditor/ckeditor5-special-characters/src/specialcharactersarrows';
+import SpecialCharactersCurrency from '@ckeditor/ckeditor5-special-characters/src/specialcharacterscurrency';
+import SpecialCharactersEssentials from '@ckeditor/ckeditor5-special-characters/src/specialcharactersessentials';
+import SpecialCharactersLatin from '@ckeditor/ckeditor5-special-characters/src/specialcharacterslatin';
+import SpecialCharactersMathematical from '@ckeditor/ckeditor5-special-characters/src/specialcharactersmathematical';
+import SpecialCharactersText from '@ckeditor/ckeditor5-special-characters/src/specialcharacterstext';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
+import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
-import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
-import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
+import TableCaption from '@ckeditor/ckeditor5-table/src/tablecaption';  
+import TableCellProperties from '@ckeditor/ckeditor5-table/src/tablecellproperties';
+import TableProperties from '@ckeditor/ckeditor5-table/src/tableproperties';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+
+import Submittable from '@telligent/ckeditor5-evolution/src/submittable';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
+	Alignment,
 	Essentials,
-	UploadAdapter,
-	Autoformat,
 	Bold,
-	Italic,
+	Clipboard,
+	Code,
+	FindAndReplace,
+	FontFamily,
+	FontColor,
+	FontBackgroundColor,
+	FontSize,
 	BlockQuote,
-	CKBox,
-	CKFinder,
-	CloudServices,
-	EasyImage,
 	Heading,
+	HorizontalLine,
 	Image,
 	ImageCaption,
 	ImageStyle,
 	ImageToolbar,
-	ImageUpload,
 	Indent,
+	IndentBlock,
+	Italic,
 	Link,
+	LinkImage,
 	List,
-	MediaEmbed,
+	ListProperties,
 	Paragraph,
 	PasteFromOffice,
-	PictureEditing,
+	RemoveFormat,
+	SelectAll,
+	SourceEditing,
+	SpecialCharacters,
+	SpecialCharactersArrows,
+	SpecialCharactersCurrency,
+	SpecialCharactersEssentials,
+	SpecialCharactersLatin,
+	SpecialCharactersMathematical,
+	SpecialCharactersText,
+	Strikethrough,
+	Subscript,
+	Superscript,
 	Table,
+	TableCaption,
+	TableCellProperties,
+	TableProperties,
 	TableToolbar,
-	TextTransformation
+	Underline,
+	
+	Submittable
 ];
 
 // Editor configuration.
@@ -71,21 +113,104 @@ ClassicEditor.defaultConfig = {
 		items: [
 			'heading',
 			'|',
+			'fontFamily',
+			'fontColor',
+			'fontSize',
+			'fontBackgroundColor',
+			'|',
 			'bold',
 			'italic',
+			'underline',
+			'strikethrough',
 			'link',
+			'subscript',
+			'superscript',
+			'code',
+			'specialCharacters',
+			'horizontalLine',
+			'|',
 			'bulletedList',
 			'numberedList',
 			'|',
 			'outdent',
 			'indent',
 			'|',
-			'uploadImage',
 			'blockQuote',
 			'insertTable',
-			'mediaEmbed',
-			'undo',
-			'redo'
+			'|',
+			'findAndReplace',
+			'removeFormat',
+			'sourceEditing'
+		],
+		shouldNotGroupWhenFull: false
+	},
+	fontFamily: {
+		options: [
+			'default',
+			'Arial, Helvetica, sans-serif',
+			'Courier New, Courier, monospace',
+			'Georgia, serif',
+			'Lucida Sans Unicode, Lucida Grande, sans-serif',
+			'Tahoma, Geneva, sans-serif',
+			'Times New Roman, Times, serif',
+			'Trebuchet MS, Helvetica, sans-serif',
+			'Verdana, Geneva, sans-serif'
+		]
+	},
+	fontColor: {
+		colors: [
+				{
+						color: 'hsl(0, 0%, 0%)',
+						label: 'Black'
+				},
+				{
+						color: 'hsl(0, 0%, 30%)',
+						label: 'Dim grey'
+				},
+				{
+						color: 'hsl(0, 0%, 60%)',
+						label: 'Grey'
+				},
+				{
+						color: 'hsl(0, 0%, 90%)',
+						label: 'Light grey'
+				},
+				{
+						color: 'hsl(0, 0%, 100%)',
+						label: 'White',
+						hasBorder: true
+				}
+		]
+	},
+	fontBackgroundColor: {
+		colors: [
+				{
+						color: 'hsl(0, 75%, 60%)',
+						label: 'Red'
+				},
+				{
+						color: 'hsl(30, 75%, 60%)',
+						label: 'Orange'
+				},
+				{
+						color: 'hsl(60, 75%, 60%)',
+						label: 'Yellow'
+				},
+				{
+						color: 'hsl(90, 75%, 60%)',
+						label: 'Light green'
+				},
+				{
+						color: 'hsl(120, 75%, 60%)',
+						label: 'Green'
+				}
+		]
+	},
+	fontSize: {
+		options: [
+				'tiny',
+				'default',
+				'big'
 		]
 	},
 	image: {
@@ -102,7 +227,9 @@ ClassicEditor.defaultConfig = {
 		contentToolbar: [
 			'tableColumn',
 			'tableRow',
-			'mergeTableCells'
+			'mergeTableCells',
+			'tableCellProperties',
+			'tableProperties'
 		]
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
